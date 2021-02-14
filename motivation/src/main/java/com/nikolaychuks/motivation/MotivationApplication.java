a1bb66ca6cdcb6a361f2a39ccfa200f7fff4ad4a
Sereggan <sergei.ni@yandex.ru>
2021-02-15 00:36:31 +0300

    Added Eureka

--- motivation/src/main/java/com/nikolaychuks/motivation/MotivationApplication.java
+++ motivation/src/main/java/com/nikolaychuks/motivation/MotivationApplication.java
@@ -2,8 +2,10 @@ package com.nikolaychuks.motivation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MotivationApplication {
 
     public static void main(String[] args) {
